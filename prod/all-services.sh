1b2ffaf39ebce65aedea43caf60cfafee13f8d68
cybabun1 <eva.evergreene@proton.me>
2025-02-18 15:59:50 -0700

    fix services in all-services, to match prod

--- prod/all-services.sh
+++ prod/all-services.sh
@@ -26,7 +26,7 @@ check_docker_compose() {
 # Run Docker Compose commands
 run_docker_compose() {
   local action="$1"
-  local project_dirs=("traefik" "services/home-staging" "services/online-staging" "services/wiki-staging")
+  local project_dirs=("traefik" "services/home" "services/online" "services/wiki" "services/danger" "services/click")
 
   case "$action" in
     "down")
